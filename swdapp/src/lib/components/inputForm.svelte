efa898c42f3707fc8fb9a93de2e9f70e7e31c2e5
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-20 16:42:32 -0600

    form button style

--- swdapp/src/lib/components/inputForm.svelte
+++ swdapp/src/lib/components/inputForm.svelte
@@ -93,4 +93,8 @@
             <input type="text" class={inputStyle} bind:value={inputValues[index]} on:input={(event) => handleChange(event, index)}>
         </div>
     {/each}
+
+    <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 w-1/4">
+        White
+    </button>
 </div>
