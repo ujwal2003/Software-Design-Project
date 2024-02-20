00a4a1e86d9412f2b3959c7c9be74e0ec926331d
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-20 13:26:04 -0600

    began input form component

---
+++ swdapp/src/lib/components/inputForm.svelte
@@ -0,0 +1,6 @@
+<div>
+    <div class="flex rounded-lg shadow-sm">
+      <span class="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:border-gray-700 dark:text-gray-400">Small</span>
+      <input type="text" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600">
+    </div>
+</div>
