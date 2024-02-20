3fe8077377fb79ddc83a391368608788bf455cdc
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-20 11:09:10 -0600

    fixed height issue

--- swdapp/src/routes/register/+page.svelte
+++ swdapp/src/routes/register/+page.svelte
@@ -3,29 +3,31 @@
     import Footer from "$lib/components/footer.svelte";
 </script>
 
-<nav>
-    <Header />
-</nav>
+<div class="flex flex-col h-screen">
+    <nav>
+        <Header />
+    </nav>
 
-<div class="overflow-hidden max-w-full overflow-x-hidden flex flex-wrap mt-14">
-    <!-- left side -->
-    <section class="w-1/2 bg-[#282828] h-full">
-        <quote class="text-white flex flex-col gap-4">
-            <p class="text-center text-wrap text-3xl font-bold italic">
-                &ldquo;My car started to fly after using FuelFinder&rdquo;
-            </p>
-            <p class="text-xl flex flex-row-reverse pr-16">
-                - Generic Car Owner
-            </p>
-        </quote>
-    </section>
+    <main class="overflow-hidden max-w-full overflow-x-hidden flex flex-wrap mt-14 h-full">
+        <!-- left side -->
+        <section class="w-1/2 bg-[#282828] h-full">
+            <quote class="text-white flex flex-col gap-4">
+                <p class="text-center text-wrap text-3xl font-bold italic">
+                    &ldquo;My car started to fly after using FuelFinder&rdquo;
+                </p>
+                <p class="text-xl flex flex-row-reverse pr-16">
+                    - Generic Car Owner
+                </p>
+            </quote>
+        </section>
 
-    <!-- right side -->
-    <section class="w-1/2 bg-[#F0F5F8]">
-        test
-    </section>
+        <!-- right side -->
+        <section class="w-1/2 bg-[#F0F5F8]">
+            test
+        </section>
+    </main>
+
+    <footer>
+        <Footer />
+    </footer>
 </div>
-
-<footer>
-    <Footer />
-</footer>
