1169c8f87360536b5c80534bbe9aef8fecfdd824
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-20 11:01:43 -0600

    basic registration page layout
    height issues, form component not started

---
+++ swdapp/src/routes/register/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Header from "$lib/components/header.svelte";
+    import Footer from "$lib/components/footer.svelte";
+</script>
+
+<nav>
+    <Header />
+</nav>
+
+<div class="overflow-hidden max-w-full overflow-x-hidden flex flex-wrap mt-14">
+    <!-- left side -->
+    <section class="w-1/2 bg-[#282828] h-full">
+        <quote class="text-white flex flex-col gap-4">
+            <p class="text-center text-wrap text-3xl font-bold italic">
+                &ldquo;My car started to fly after using FuelFinder&rdquo;
+            </p>
+            <p class="text-xl flex flex-row-reverse pr-16">
+                - Generic Car Owner
+            </p>
+        </quote>
+    </section>
+
+    <!-- right side -->
+    <section class="w-1/2 bg-[#F0F5F8]">
+        test
+    </section>
+</div>
+
+<footer>
+    <Footer />
+</footer>
