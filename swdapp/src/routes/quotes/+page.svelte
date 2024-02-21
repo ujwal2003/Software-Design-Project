c8bbe8b0c33a15e76e69a19be7d0206a4891d16b
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-21 13:30:13 -0600

    base page for quote history

--- swdapp/src/routes/quotes/+page.svelte
+++ swdapp/src/routes/quotes/+page.svelte
@@ -9,7 +9,30 @@
     </nav>
 
     <main class="overflow-hidden max-w-full overflow-x-hidden flex flex-wrap mt-14 h-full">
-        test
+        <!-- left sidebar -->
+        <aside class="w-1/6 bg-[#282828] h-full flex justify-left pl-10 pt-6 text-lg">
+            <nav class="flex flex-col gap-2">
+                <a href="/" class="text-[#CBD5E1]"> 
+                    Profile 
+                </a>
+    
+                <a href="/" class="text-[#CBD5E1]"> 
+                    Payment History 
+                </a>
+            </nav>
+        </aside>
+
+        <!-- main content -->
+        <div class="w-5/6 bg-[#F0F5F8] flex flex-col flex-wrap">
+            <p class="text-4xl pl-8 pt-4">
+                Fuel Quote History
+            </p>
+
+            <div>
+                <!-- TODO: Quote Cards -->
+                <!-- TODO: Quote Detail -->
+            </div>
+        </div>
     </main>
 
     <footer>
