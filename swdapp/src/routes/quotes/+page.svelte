5ea0aca58fc1152d23616597dd36d67333e43aa6
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-21 11:22:08 -0600

    base template for quote history page

---
+++ swdapp/src/routes/quotes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Header from "$lib/components/header.svelte";
+    import Footer from "$lib/components/footer.svelte";
+</script>
+
+<div class="flex flex-col h-screen">
+    <nav>
+        <Header />
+    </nav>
+
+    <main class="overflow-hidden max-w-full overflow-x-hidden flex flex-wrap mt-14 h-full">
+        test
+    </main>
+
+    <footer>
+        <Footer />
+    </footer>
+</div>
