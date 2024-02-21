209af81fcf8b3b996ca795419c88697803e83377
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-20 19:50:24 -0600

    button naming in form component

--- swdapp/src/lib/components/inputForm.svelte
+++ swdapp/src/lib/components/inputForm.svelte
@@ -21,6 +21,7 @@
     export let darkMode: boolean = false;
     export let labels: string[];
     export let numInputs: number;
+    export let btnLabel: string;
 
     
     interface InputValue {
@@ -94,7 +95,9 @@
         </div>
     {/each}
 
-    <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 w-1/4">
-        White
-    </button>
+    <div class="flex justify-end">
+        <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+            {btnLabel}
+        </button>
+    </div>
 </div>
