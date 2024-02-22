ba8bf15831e7ad647451471c01cefcae701bc398
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-22 13:32:42 -0600

    base code for receipts page

---
+++ swdapp/src/routes/receipts/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Header from "$lib/components/header.svelte";
+    import Footer from "$lib/components/footer.svelte";
+
+    import { dummyQuoteData } from "$lib";
+</script>
+
+<div class="flex flex-col h-screen">
+    <nav>
+		<Header />
+	</nav>
+
+    <main class="overflow-hidden max-w-full overflow-x-hidden flex flex-wrap mt-0 h-full">
+        Page Under Construction.
+    </main>
+
+    <footer>
+        <Footer />
+    </footer>
+</div>
