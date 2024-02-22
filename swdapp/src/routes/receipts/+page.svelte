97584b3f52b74026df89cb80515b518b39151cc8
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-22 16:25:48 -0600

    base layout for receipts page

--- swdapp/src/routes/receipts/+page.svelte
+++ swdapp/src/routes/receipts/+page.svelte
@@ -11,7 +11,27 @@
 	</nav>
 
     <main class="overflow-hidden max-w-full overflow-x-hidden flex flex-wrap mt-0 h-full">
-        Page Under Construction.
+        <!-- left sidebar -->
+		<aside class="w-1/6 bg-[#282828] h-full flex justify-left pl-10 pt-6 text-lg">
+			<nav class="flex flex-col gap-2">
+				<a href="/" class="text-[#CBD5E1]"> Profile </a>
+				<a href="/" class="text-[#CBD5E1]"> Quote History </a>
+			</nav>
+		</aside>
+
+        <div class="w-5/6 bg-[#F0F5F8] flex flex-col flex-wrap">
+			<p class="text-3xl pl-8 pt-4">Payment History</p>
+
+			<div class="flex">
+				<div class="w-1/3 pl-7 pt-4">
+					test
+				</div>
+
+				<div class="w-2/3 ml-6 mr-6 mt-4">
+					test
+				</div>
+			</div>
+		</div>
     </main>
 
     <footer>
