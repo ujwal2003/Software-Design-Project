e2b0354e35e0a62dea8a278e7eb788a0dba424b1
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-23 08:05:37 -0600

    added selector component

--- swdapp/src/routes/test/+page.svelte
+++ swdapp/src/routes/test/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
     import DatePicker from "$lib/components/datePicker.svelte";
+    import Selector from "$lib/components/selector.svelte";
 </script>
 
-<div class="flex justify-center">
+<div class="flex flex-col items-center">
+    <Selector label={"label"}>
+        <option>Option 1</option>
+        <option>Option 2</option>
+        <option>Option 3</option>
+    </Selector>
     <DatePicker />
 </div>
