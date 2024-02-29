316dc0cd89d37aed1dc2b5665a21add6503fc3b0
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-28 21:13:29 -0600

    cards component demo

--- swdapp/src/routes/test/+page.svelte
+++ swdapp/src/routes/test/+page.svelte
@@ -25,61 +25,34 @@
 	import Card from "$lib/components/cards/card.svelte";
 	import CardText from "$lib/components/cards/cardText.svelte";
 
+	const cards = [
+		{_id: "1", title: "title 1", text: "lorem ipsum dolor"},
+		{_id: "2", title: "title 2", text: "abcdefghijklmnopqrstuvwxyz"},
+		{_id: "3", title: "title 3", text: "description"},
+		{_id: "4", title: "title 4", text: "text"},
+		{_id: "5", title: "title 5", text: "card text"},
+	]
+
 	function handleCardClick(e: CustomEvent<any>) {
 		console.log(e.detail);
 	}
 </script>
 
 <main class="flex justify-center mt-14">
-	<div class="w-1/2">
+	<div class="w-1/4">
 
 		<CardContainer>
-			<Card cardID="1" btnName={"Card Details"} on:cardClick={(e) => {handleCardClick(e)}}>
-				<CardText title={true}>
-					Title
-				</CardText>
-				<CardText>
-					description
-				</CardText>
-			</Card>
-
-			<Card cardID="2" btnName={"Card Details"} on:cardClick={(e) => {handleCardClick(e)}}>
-				<CardText title={true}>
-					Title
-				</CardText>
-				<CardText>
-					description
-				</CardText>
-			</Card>
-
-			<Card cardID="3" btnName={"Card Details"} on:cardClick={(e) => {handleCardClick(e)}}>
-				<CardText title={true}>
-					Title
-				</CardText>
-				<CardText>
-					description
-				</CardText>
-			</Card>
-
-			<Card cardID="4" btnName={"Card Details"} on:cardClick={(e) => {handleCardClick(e)}}>
-				<CardText title={true}>
-					Title
-				</CardText>
-				<CardText>
-					description
-				</CardText>
-			</Card>
-
-			<Card cardID="5" btnName={"Card Details"} on:cardClick={(e) => {handleCardClick(e)}}>
-				<CardText title={true}>
-					Title
-				</CardText>
-				<CardText>
-					description
-				</CardText>
-			</Card>
+			{#each cards as card}
+				<Card cardID={card._id} btnName={"Card Details"} on:cardClick={(e) => {handleCardClick(e)}}>
+					<CardText title={true}>
+						{card.title}
+					</CardText>
+					<CardText>
+						{card.text}
+					</CardText>
+				</Card>
+			{/each}
 		</CardContainer>
-
 
 	</div>
 </main>
