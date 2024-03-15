807c5f0c0a20334d352a552a9f97f8fba9b79946
ujwal2003 <joshiujwal2003@gmail.com>
2024-03-15 18:51:09 -0500

    implemented user exists check

---
+++ swdapp/src/lib/server/controllers/userController.ts
@@ -0,0 +1,7 @@
+import { dummyUsersModel } from "../dummyDatabase";
+
+export async function userExists(username: string) {
+    const foundUser = dummyUsersModel.find(user => user.username === username);
+
+    return foundUser ? true: false;
+}
--- swdapp/src/routes/api/auth/register/+server.ts
+++ swdapp/src/routes/api/auth/register/+server.ts
@@ -1,9 +1,22 @@
 import * as bcrypt from 'bcrypt';
 import { json } from '@sveltejs/kit';
 
+import { userExists } from '$lib/server/controllers/userController.js';
+
 interface RegistrationRequest {
     username: string,
     password: string
+};
+
+interface FailResponse {
+    failType: "error" | "exists",
+    message: string,
+    err?: any
+}
+
+interface RegistrationResponse {
+    success: boolean,
+    response: string | FailResponse
 };
 
 export async function POST({request}): Promise<Response> {
@@ -12,12 +25,22 @@ export async function POST({request}): Promise<Response> {
     
         console.log("[SERVER] POST /api/auth/register recieved: ", body);
     
-        //TODO: check if user already exists
+        if(await userExists(body.username)) {
+            return json({
+                success: false,
+                response: {
+                    failType: 'exists',
+                    message: `${body.username} is already registered`
+                }
+            } as RegistrationResponse, {status: 500});
+        }
+
+        //TODO: add user to "database"
     
         return json({
             success: true,
             response: `Succesfully registered ${body.username}`
-        }, {status: 201});
+        } as RegistrationResponse, {status: 201});
         
     } catch (error) {
         console.log("[SERVER] error on POST /api/auth/register", error);
@@ -25,9 +48,10 @@ export async function POST({request}): Promise<Response> {
         return json({
             success: false,
             response: {
+                failType: 'error',
                 message: "Request failed due to error",
                 err: error
             }
-        }, {status: 500});
+        } as RegistrationResponse, {status: 500});
     }
 }
