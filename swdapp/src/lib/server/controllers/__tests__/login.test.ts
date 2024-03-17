7ab70f9778aebdd9c68d2f58a49394944577eac5
ujwal2003 <joshiujwal2003@gmail.com>
2024-03-16 22:08:26 -0500

    defined todo unit tests for login route

---
+++ swdapp/src/lib/server/controllers/__tests__/login.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from 'vitest';
+
+test.todo('succesful login for user', async () => {
+
+})
+
+test.todo('unsuccesful login (user does not exist)', async () => {
+
+})
+
+test.todo('unsucessful login (wrong password)', async () => {
+
+})
+
+test.todo('authentication fails due to error or invalid info', async () => {
+    
+})
