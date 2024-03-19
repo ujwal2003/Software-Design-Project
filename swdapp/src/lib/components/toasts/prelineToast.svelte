4edaef393ef853828409ea6325a1085519739f8f
ujwal2003 <joshiujwal2003@gmail.com>
2024-03-19 17:31:21 -0500

    started custom toast component

---
+++ swdapp/src/lib/components/toasts/prelineToast.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    type ToastType = "success" | "failure" | "generic";
+    export let toastType: ToastType = "generic";
+
+    export let toastText: string;
+
+    type TailwindString = string;
+    interface ToastStyle {
+      background: TailwindString,
+      border_color: TailwindString,
+      text_color: TailwindString
+    };
+
+    export let customToastStyle: ToastStyle | {} = {};
+
+    let toastStyles: string;
+    if(customToastStyle) {
+      // toastStyles = `max-w-xs ${customToastStyle.background}`
+    } else if(toastType == "generic") {
+      toastStyles = "max-w-xs bg-blue-100 border border-blue-200 text-sm text-blue-800 rounded-lg";
+    } else if(toastType == "success") {
+      toastStyles = "max-w-xs bg-teal-100 border border-teal-200 text-sm text-teal-800 rounded-lg";
+    } else if(toastType == "failure") {
+      toastStyles = "max-w-xs bg-red-100 border border-red-200 text-sm text-red-800 rounded-lg";
+    }
+</script>
+
+<div class={toastStyles} role="alert">
+    <div class="flex p-4">
+      {toastText}
+    </div>
+</div>
--- swdapp/src/routes/test/+page.svelte
+++ swdapp/src/routes/test/+page.svelte
@@ -1,58 +1,7 @@
 <script lang="ts">
-	//   import InputForm from "$lib/components/inputForm.svelte";
-
-	//   let darkMode = false;
-	//   let labels = ['Label 1', 'Label 2', 'Label 3'];
-	//   let numInputs = 3;
-	//   let fromDescription = {
-	//     title: 'Full Name',
-	//     button: 'Submit',
-	//     top: '',
-	//     bottom: ''
-	//   };
-
-	//   function handleInputChange(e: any) {
-	//     console.log('Input changed:', e.detail);
-	//   }
-
-	//   function handleFormSubmit() {
-	//     console.log('Form submitted');
-	//   }
-
-	let isOpen = false;
-
-	import CardContainer from "$lib/components/cards/cardContainer.svelte";
-	import Card from "$lib/components/cards/card.svelte";
-	import CardText from "$lib/components/cards/cardText.svelte";
-
-	const cards = [
-		{_id: "1", title: "title 1", text: "lorem ipsum dolor"},
-		{_id: "2", title: "title 2", text: "abcdefghijklmnopqrstuvwxyz"},
-		{_id: "3", title: "title 3", text: "description"},
-		{_id: "4", title: "title 4", text: "text"},
-		{_id: "5", title: "title 5", text: "card text"},
-	]
-
-	function handleCardClick(e: CustomEvent<any>) {
-		console.log(e.detail);
-	}
+	import PrelineToast from "$lib/components/toasts/prelineToast.svelte";
 </script>
 
 <main class="flex justify-center mt-14">
-	<div class="w-1/4">
-
-		<CardContainer>
-			{#each cards as card}
-				<Card cardID={card._id} btnName={"Card Details"} on:cardClick={(e) => {handleCardClick(e)}}>
-					<CardText title={true}>
-						{card.title}
-					</CardText>
-					<CardText>
-						{card.text}
-					</CardText>
-				</Card>
-			{/each}
-		</CardContainer>
-
-	</div>
+	<PrelineToast toastText="toast text" toastType="failure" />
 </main>
