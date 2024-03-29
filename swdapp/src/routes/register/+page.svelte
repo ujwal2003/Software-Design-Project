c66cbb4b1494b0b55213cf1565ae1d31da390954
ujwal2003 <joshiujwal2003@gmail.com>
2024-03-28 23:57:44 -0500

    implemented new form component into register

--- swdapp/src/routes/register/+page.svelte
+++ swdapp/src/routes/register/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
-	import InputForm from '$lib/components/inputForm.svelte';
+
+	// import InputForm from '$lib/components/inputForm.svelte';
+	import SubmitForm from '$lib/components/submission-form/submitForm.svelte';
+	import FormText from '$lib/components/submission-form/formText.svelte';
+	import FormInput from '$lib/components/submission-form/formInput.svelte';
+	import FormButton from '$lib/components/submission-form/formButton.svelte';
+
 	import { failureAlert, successAlert } from '$lib/components/toasts/customToasts';
 	import { postRequest } from '$lib/requests';
 	import { goto } from '$app/navigation';
@@ -9,6 +15,7 @@
 	const regTopDescription = 'Make a new account to access gas savings you could only dream of!';
 	const regBottomDescription = 'Already have an account? <a href="/login"><u>Log in here.</u></a>';
 
+	/*
 	interface InputValue {
 		inputLabel: string;
 		inputValue: string;
@@ -51,6 +58,43 @@
 		successAlert('Registration Succesful! Redirecting...');
 		goto('/login/verify/');
 	}
+	*/
+
+	let registerRequest = {
+		username: '',
+		password: ''
+	};
+
+	type RegLabel = "username" | "password";
+	function handleRegInputChange(e: any, inpType: RegLabel): void {
+		registerRequest = {
+			username: (inpType == 'username') ? e.detail : registerRequest.username,
+			password: (inpType == 'password') ? e.detail : registerRequest.password
+		}
+	}
+
+	async function handleRegistrationSubmit() {
+		if(!registerRequest.username || !registerRequest.password) {
+			failureAlert("Registration form must be completely filled out!");
+			return;
+		}
+
+		const registerRes = await postRequest('api/auth/register', registerRequest);
+		const resJSON = await registerRes.json();
+
+		if(!resJSON.success) {
+			if(resJSON.response.failType == "exists") {
+				failureAlert(resJSON.response.message);
+				return;
+			}
+
+			failureAlert("Registration failed due to error, please try again");
+			return;
+		}
+
+		successAlert('Registration Succesful! Redirecting...');
+		goto('/login/verify/');
+	}
 </script>
 
 <nav>
@@ -71,7 +115,7 @@
 	<!-- right side -->
 	<section class="flex h-screen w-1/2 flex-wrap items-center justify-center bg-[#F0F5F8]">
 		<div class="w-1/2">
-			<InputForm
+			<!-- <InputForm
 				numInputs={3}
 				labels={['Email', 'Username', 'Password']}
 				inputTypes={['email', 'text', 'password']}
@@ -84,7 +128,20 @@
 				bind:formInputValues={registrationVals}
 				on:inputChange={handleRegInputChange}
 				on:formSubmit={handleRegistrationSubmit}
-			/>
+			/> -->
+			<SubmitForm>
+				<FormText title>Create Account</FormText>
+				<FormText description>{regTopDescription}</FormText>
+
+				<FormInput inputType='text' placeholderText='Username' on:formInput={e => {handleRegInputChange(e, 'username')}} />
+				<FormInput inputType='password' placeholderText='Password' on:formInput={e => {handleRegInputChange(e, 'password')}} />
+
+				<FormButton justify='end' on:formClick={handleRegistrationSubmit}>
+					Register
+				</FormButton>
+
+				<FormText description>{@html regBottomDescription}</FormText>
+			</SubmitForm>
 		</div>
 	</section>
 </main>
