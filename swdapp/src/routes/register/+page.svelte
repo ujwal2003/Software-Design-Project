6d1a26dedfd953e537d5043535711b89bf2cb725
ujwal2003 <joshiujwal2003@gmail.com>
2024-03-28 23:58:30 -0500

    removed old form component logic from register

--- swdapp/src/routes/register/+page.svelte
+++ swdapp/src/routes/register/+page.svelte
@@ -2,7 +2,6 @@
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
 
-	// import InputForm from '$lib/components/inputForm.svelte';
 	import SubmitForm from '$lib/components/submission-form/submitForm.svelte';
 	import FormText from '$lib/components/submission-form/formText.svelte';
 	import FormInput from '$lib/components/submission-form/formInput.svelte';
@@ -14,51 +13,6 @@
 
 	const regTopDescription = 'Make a new account to access gas savings you could only dream of!';
 	const regBottomDescription = 'Already have an account? <a href="/login"><u>Log in here.</u></a>';
-
-	/*
-	interface InputValue {
-		inputLabel: string;
-		inputValue: string;
-	}
-
-	let registrationVals: InputValue[] = [];
-	function handleRegInputChange(e: any) {
-		registrationVals = e.detail;
-	}
-
-	async function handleRegistrationSubmit() {
-		if(registrationVals.length < 3 || !registrationVals[0].inputValue || !registrationVals[1].inputValue || !registrationVals[2].inputValue) {
-			failureAlert("Registration form must be completely filled out!");
-			return;
-		}
-
-		const registrationFormInfo = {
-			email: registrationVals[0].inputValue,
-			username: registrationVals[1].inputValue,
-			password: registrationVals[2].inputValue
-		};
-
-		const registerRes = await postRequest('api/auth/register', {
-			username: registrationFormInfo.username,
-			password: registrationFormInfo.password
-		});
-
-		const resJSON = await registerRes.json();
-
-		if(!resJSON.success) {
-			if(resJSON.response.failType == "exists") {
-				failureAlert(resJSON.response.message);
-				return;
-			}
-
-			failureAlert("Registration failed due to error, please try again");
-			return;
-		}
-
-		successAlert('Registration Succesful! Redirecting...');
-		goto('/login/verify/');
-	}
-	*/
 
 	let registerRequest = {
 		username: '',
@@ -115,20 +69,6 @@
 	<!-- right side -->
 	<section class="flex h-screen w-1/2 flex-wrap items-center justify-center bg-[#F0F5F8]">
 		<div class="w-1/2">
-			<!-- <InputForm
-				numInputs={3}
-				labels={['Email', 'Username', 'Password']}
-				inputTypes={['email', 'text', 'password']}
-				fromDescription={{
-					title: 'Create Account',
-					button: 'Register',
-					top: regTopDescription,
-					bottom: regBottomDescription
-				}}
-				bind:formInputValues={registrationVals}
-				on:inputChange={handleRegInputChange}
-				on:formSubmit={handleRegistrationSubmit}
-			/> -->
 			<SubmitForm>
 				<FormText title>Create Account</FormText>
 				<FormText description>{regTopDescription}</FormText>
