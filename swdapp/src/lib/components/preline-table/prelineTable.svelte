3fd5163a61a2d4b1529c8e73091e1b536c1ff249
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-02 13:29:38 -0500

    base preline hover table component
    removed dark mode

---
+++ swdapp/src/lib/components/preline-table/prelineTable.svelte
@@ -0,0 +1,64 @@
+<div class="flex flex-col">
+    <div class="-m-1.5 overflow-x-auto">
+      <div class="p-1.5 min-w-full inline-block align-middle">
+        <div class="overflow-hidden">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead>
+              <tr>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Name</th>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Age</th>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Address</th>
+                <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Action</th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200">
+              <tr class="hover:bg-gray-100">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">John Brown</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">45</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">New York No. 1 Lake Park</td>
+                <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                  <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+                </td>
+              </tr>
+  
+              <tr class="hover:bg-gray-100">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">Jim Green</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">27</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">London No. 1 Lake Park</td>
+                <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                  <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+                </td>
+              </tr>
+  
+              <tr class="hover:bg-gray-100">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">Joe Black</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">31</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">Sidney No. 1 Lake Park</td>
+                <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                  <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+                </td>
+              </tr>
+  
+              <tr class="hover:bg-gray-100">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">Edward King</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">16</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">LA No. 1 Lake Park</td>
+                <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                  <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+                </td>
+              </tr>
+  
+              <tr class="hover:bg-gray-100">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">Jim Red</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">45</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">Melbourne No. 1 Lake Park</td>
+                <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                  <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
