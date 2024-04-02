e6e0f81db438907d19cfc8c935c5d3d553bede72
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-02 14:04:30 -0500

    added border and head background

--- swdapp/src/lib/components/preline-table/tableSection.svelte
+++ swdapp/src/lib/components/preline-table/tableSection.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
     type StyleType = 'head' | 'body';
     export let style: StyleType;
+
+    type TailwindString = string;
+    export let headBg: TailwindString = 'bg-gray-50';
 </script>
 
 {#if style === 'head'}
-    <thead>
+    <thead class={headBg}>
         <slot></slot>
     </thead>
 {:else}
