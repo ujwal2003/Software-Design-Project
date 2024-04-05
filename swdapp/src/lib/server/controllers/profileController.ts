6499d222e0b169f7147316372d15837cd45db475
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-04 21:25:52 -0500

    moved payment methods into payment service

--- swdapp/src/lib/server/controllers/profileController.ts
+++ swdapp/src/lib/server/controllers/profileController.ts
@@ -1,5 +1,8 @@
 import { json } from '@sveltejs/kit';
-import { getProfile, getQuoteHistory, getPurchaseHistory, generateQuote, updateAccount, makePayment, updatePayment } from '../services/userService';
+import { getProfile, getQuoteHistory, generateQuote, updateAccount } from '../services/userService';
+import { makePayment } from "../services/paymentService";
+import { updatePayment } from "../services/paymentService";
+import { getPurchaseHistory } from "../services/paymentService";
 import type { GeneralAPIResponse, ProfileRequest, ProfileResponse, QuoteHistoryRequest, QuoteHistoryResponse, PurchaseHistoryResponse, UnauthorizedResponse, GenerateQuoteRequest, GenerateQuoteResponse, UpdateAccountRequest, MakePaymentRequest } from '../customTypes/generalTypes';
 import { isAccessTokenValid_simple } from './authController';
 
--- swdapp/src/lib/server/services/userService.ts
+++ swdapp/src/lib/server/services/userService.ts
@@ -1,5 +1,5 @@
 import crypto from "crypto";
-import { dummyUsersModel, dummyRefreshTokens, dummyCompanyModel } from "../dummyDatabase";
+import { dummyUsersModel, dummyRefreshTokens } from "../dummyDatabase";
 
 export async function userExists(username: string) {
     const foundUser = dummyUsersModel.find(user => user.username === username);
@@ -59,13 +59,6 @@ export async function getQuoteHistory(username: string){
     
 }
 
-export async function getPurchaseHistory(username: string){
-    const user = dummyUsersModel.find(user => user.username === username);
-
-    return user ? user.profile?.purchaseHistory : null;
-    
-}
-
 export async function generateQuote(username: string, gallonsRequested: number, deliveryDate: string, loc: string){
     const user = dummyUsersModel.find(user => user.username === username);
     const quoteHistory = user ? user.profile?.quoteHistory : null;
@@ -121,60 +114,3 @@ export async function updateAccount(username: string, firstName?: string, middle
     return profile;
 }
 
-export async function updatePayment(username: string, cardName?: string, cardNum?: string, cvv?: string, expiry?: Date) {
-    const user = dummyUsersModel.find(user => user.username === username);
-    if(!user || !user.profile)
-        return false;
-
-    let userPayment = user.profile.paymentInfo;
-
-    if(!userPayment) {
-        userPayment = {
-            _id: crypto.randomBytes(24 / 2).toString('hex'),
-            cardName: '',
-            creditCardNumber: '',
-            cardCVV: '',
-            cardExpiration: new Date(-8640000000000000)
-        }
-    }
-
-    userPayment.cardName = cardName ? cardName : userPayment.cardName;
-    userPayment.creditCardNumber = cardNum ? cardNum : userPayment.creditCardNumber;
-    userPayment.cardCVV = cvv ? cvv : userPayment.cardCVV;
-    userPayment.cardExpiration = expiry ? expiry : userPayment.cardExpiration;
-
-    return true;
-}
-
-export async function makePayment(username: string, price: number, companyName: string){
-    const user = dummyUsersModel.find(user => user.username === username);
-    const paymentInfo = user ? user.profile?.paymentInfo : null;
-
-    if (!paymentInfo){
-        return;
-    }
-
-    //beep beep credit card machine
-    //imagine the credit card is being processed
-    //$$$$$$$
-
-    const company = dummyCompanyModel.find(cn => cn.name === companyName);
-
-    if (!company){
-        console.log("Company not found");
-        return;
-    }
-    
-    company.revenue += price;
-    let purchase = {
-        _id: crypto.randomBytes(24 / 2).toString('hex'),
-        quoteID: "",
-        purchaseDate: new Date(),
-        deliveryDate: new Date(),
-        tax: (0.0625 * price),
-        price: price - (0.0625 * price)
-    }
-    user?.profile?.purchaseHistory?.push(purchase);
-
-    return paymentInfo;
-}
