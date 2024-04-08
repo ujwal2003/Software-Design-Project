0fecc960a31443386ff2811d48ff250e70da30ae
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-08 17:12:11 -0500

    added db implementation to adding and revoking refresh tokens

--- swdapp/src/lib/server/services/authorizationService.ts
+++ swdapp/src/lib/server/services/authorizationService.ts
@@ -1,5 +1,5 @@
 import { dummyRefreshTokens, dummyUsersModel } from "../dummyDatabase";
-
+import { AuthorizationModel } from "../database/models/authorizeModel";
 
 export async function getUserCredentials(username: string) {
     const foundUser = dummyUsersModel.find(user => user.username === username);
@@ -11,17 +11,21 @@ export async function getUserCredentials(username: string) {
 }
 
 export async function addUserRefreshSession(refreshToken: string) {
-    dummyRefreshTokens.push(refreshToken);
+    dummyRefreshTokens.push(refreshToken); //! Remove this line after revoke function is implemented
+
+    const session = await AuthorizationModel.create({ refToken: refreshToken });
 }
 
 export async function revokeRefreshToken(refreshToken: string) {
+    //! Remove these lines after confirmation of db working
     const indexToDelete = dummyRefreshTokens.indexOf(refreshToken);
-
     if (indexToDelete !== -1)
         dummyRefreshTokens.splice(indexToDelete, 1);
+
+    const revokeSession = await AuthorizationModel.deleteOne({ refToken: refreshToken });
 }
+
 export async function isRefreshTokenValid(refreshToken: string) {
     const foundToken = dummyRefreshTokens.find(token => token === refreshToken);
     return foundToken ? true : false;
 }
-
