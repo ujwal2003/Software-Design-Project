bf74aa45aad4c72ae2b6f1ab8ff333c5e02b6ce2
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-08 17:30:16 -0500

    removed dummy db references from token methods in service
    marked user credentials method for refactoring

--- swdapp/src/lib/server/services/authorizationService.ts
+++ swdapp/src/lib/server/services/authorizationService.ts
@@ -2,6 +2,7 @@ import { dummyRefreshTokens, dummyUsersModel } from "../dummyDatabase";
 import { AuthorizationModel } from "../database/models/authorizeModel";
 
 export async function getUserCredentials(username: string) {
+    //! Refactor this function when implementing user schema
     const foundUser = dummyUsersModel.find(user => user.username === username);
 
     return {
@@ -11,25 +12,14 @@ export async function getUserCredentials(username: string) {
 }
 
 export async function addUserRefreshSession(refreshToken: string) {
-    dummyRefreshTokens.push(refreshToken); //! Remove this line after revoke function is implemented
-
     const session = await AuthorizationModel.create({ refToken: refreshToken });
 }
 
 export async function revokeRefreshToken(refreshToken: string) {
-    //! Remove these lines after confirmation of db working
-    const indexToDelete = dummyRefreshTokens.indexOf(refreshToken);
-    if (indexToDelete !== -1)
-        dummyRefreshTokens.splice(indexToDelete, 1);
-
     const revokeSession = await AuthorizationModel.deleteOne({ refToken: refreshToken });
 }
 
 export async function isRefreshTokenValid(refreshToken: string) {
-    //! Remove these lines after confirmation of db working
-    // const foundToken = dummyRefreshTokens.find(token => token === refreshToken);
-    // return foundToken ? true : false;
-
     const validToken = await AuthorizationModel.findOne({ refToken: refreshToken }).exec();
     // console.log(validToken);
     
