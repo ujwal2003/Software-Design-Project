b4624f5f240f94dde1b4048ddfadfb0bbb1814fe
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-11 23:52:30 -0500

    refactored 2 unit tests for getQuote test suite

--- swdapp/src/lib/server/controllers/__tests__/getQuotes.test.ts
+++ swdapp/src/lib/server/controllers/__tests__/getQuotes.test.ts
@@ -1,9 +1,17 @@
-import { beforeAll, expect, test, vi } from 'vitest';
-
+import { afterAll, beforeAll, expect, test, vi } from 'vitest';
+import * as bcrypt from "bcrypt";
 import { getQuoteHistoryData } from '../profileController';
-import type { QuoteHistoryRequest, QuoteHistoryResponse, GeneralAPIResponse, UnauthorizedResponse } from '$lib/server/customTypes/generalTypes';
+import * as UserService from '../../services/userService';
+import * as AuthService from "../../services/authorizationService";
+import * as QuoteService from "../../services/quoteService";
 import type { LoginRequest, LoginResponse, LoginSuccess } from '$lib/server/customTypes/authTypes';
 import { loginUser } from '../authController';
+import type { GeneralAPIResponse, QuoteHistoryRequest } from '$lib/server/customTypes/generalTypes';
+
+const userExistsSpy = vi.spyOn(UserService, 'userExists');
+const getCredsSpy = vi.spyOn(AuthService, 'getUserCredentials');
+const addRefTokenSpy = vi.spyOn(AuthService, 'addUserRefreshSession');
+const quoteHistorySpy = vi.spyOn(QuoteService, 'getQuoteHistory');
 
 beforeAll(() => {
     vi.mock('$env/static/private', () => {
@@ -12,122 +20,70 @@ beforeAll(() => {
             ACCESS_TOKEN_SECRET: 'test2'
         }
     });
-})
+});
 
-test.skip('successful quote history test', async () => {
-    const testLoginRequest: LoginRequest = {
-        username: 'dummyUser3',
-        password: 'unsecurePassword3'
-    }
+afterAll(() => {
+    userExistsSpy.mockRestore();
+    getCredsSpy.mockRestore();
+    addRefTokenSpy.mockRestore();
+    quoteHistorySpy.mockRestore();
+});
+
+test('successful quote history', async () => {
+    const testLoginRequest: LoginRequest = { username: 'dummyUser', password: 'pass1' };
+
+    const salt = await bcrypt.genSalt();
+    const hashedPass = await bcrypt.hash(testLoginRequest.password, salt);
+
+    (userExistsSpy as any).mockImplementation(async () => { return true; });
+    getCredsSpy.mockImplementation(async () => { return { username: 'user1', encryptedPass: hashedPass }; });
+    addRefTokenSpy.mockImplementation(async () => { return; });
 
     const loginRes: LoginResponse<LoginSuccess> = await (await loginUser(testLoginRequest)).json();
+    expect(loginRes.success).toBeTruthy();
 
     const testRequest: QuoteHistoryRequest = {
-        username: 'dummyUser3',
+        username: testLoginRequest.username,
         accessToken: loginRes.response.accessToken
-    }
+    };
 
-    expect(await (await getQuoteHistoryData(testRequest)).json()).toEqual({
-        success: true,
-        quoteHistory: [
-            {
-                _id: "9e8e9a6bcc756b25eb1bef22",
-                generationDate: "2024-02-25T00:00:00.000Z", // Adjusted to match the format of actual data
-                gallonsRequested: 5,
-                priceCalculated: 2.65
-            },
-            {
-                _id: "a53b59be0a4182fb778de339",
-                generationDate: "2024-01-20T00:00:00.000Z", // Adjusted to match the format of actual data
-                gallonsRequested: 3,
-                priceCalculated: 3.65
-            },
-            {
-                _id: "8272a0022599956bcb1659ee",
-                generationDate: "2023-01-15T00:00:00.000Z", // Adjusted to match the format of actual data
-                gallonsRequested: 5,
-                priceCalculated: 1.65
-            }
-        ] 
+    (quoteHistorySpy as any).mockImplementation(async () => {
+        return [
+            { generationDate: new Date("2024-04-11"), gallonsRequested: 5, priceCalculated: 2.65 },
+            { generationDate: new Date("2024-04-10"), gallonsRequested: 3, priceCalculated: 3.45 },
+            { generationDate: new Date("2024-04-09"), gallonsRequested: 7, priceCalculated: 1.75 }
+        ];
     });
-    
 
-})
+    const res = await getQuoteHistoryData(testRequest);
+    const resJSON = await res.json();
 
-test.skip('successful quote history test (empty / no quotes)', async () => {
-    const testLoginRequest: LoginRequest = {
-        username: 'dummyUser2',
-        password: 'unsecurePassword2'
-    }
+    expect(resJSON.success).toBeTruthy();
+    expect(resJSON.quoteHistory).toBeDefined();
+    expect(resJSON.quoteHistory.length).toEqual(3);
+    expect(resJSON.quoteHistory[0]).toBeTypeOf('object');
+});
 
-    const loginRes: LoginResponse<LoginSuccess> = await (await loginUser(testLoginRequest)).json();
+test.todo('succesful quote history for user with now quotes', async () => {
+    //TODO implementation
+});
 
-    const testRequest: QuoteHistoryRequest = {
-        username: 'dummyUser2',
-        accessToken: loginRes.response.accessToken
-    }
+test.todo('failure to get quotes because user does not have a profile set', async () => {
+    //TODO implementation
+});
 
-    expect(await (await getQuoteHistoryData(testRequest)).json()).toEqual({
-            success: true,
-            quoteHistory: [] 
-    } as QuoteHistoryResponse);
-})
+test.todo('unsuccesful quote retrieval due to invalid access token', async () => {
+    //TODO implementation
+});
 
-test.skip('profile not found test', async () => {
-    const testLoginRequest: LoginRequest = {
-        username: 'dummyUser1',
-        password: 'unsecurePassword1'
-    }
+test.todo('unsuccesful quote retrieval due to internal error', async () => {
+    //TODO implementation
+});
 
-    const loginRes: LoginResponse<LoginSuccess> = await (await loginUser(testLoginRequest)).json();
-
-    const testRequest: QuoteHistoryRequest = {
-        username: 'dummyUser1',
-        accessToken: loginRes.response.accessToken
-    }
-
-    expect(await (await getQuoteHistoryData(testRequest)).json()).toEqual({
-        success: false,
-        message: "Quote history not found"
-    } as GeneralAPIResponse);
-})
-
-test.skip('user not found test', async () => {
-    const testLoginRequest: LoginRequest = {
-        username: 'dummyUser1',
-        password: 'unsecurePassword1'
-    }
-
-    const loginRes: LoginResponse<LoginSuccess> = await (await loginUser(testLoginRequest)).json();
-
-    const testRequest: QuoteHistoryRequest = {
-        username: 'dummyUserNew',
-        accessToken: loginRes.response.accessToken
-    }
-
-    expect(await (await getQuoteHistoryData(testRequest)).json()).toEqual({
-        success: false,
-        message: "Quote history not found"
-    } as GeneralAPIResponse);
-})
-
-test.skip('unsuccesful quote retrieval due to invalid access token', async () => {
-    const testRequest: QuoteHistoryRequest = {
-        username: 'dummyUser3',
-        accessToken: ''
-    }
-
-    expect(await (await getQuoteHistoryData(testRequest)).json()).toEqual({
-        success: true,
-        unauthorized: true,
-        message: 'invalid access token'
-    } as UnauthorizedResponse);
-})
-
-test.skip('unsuccesful quote retrieval due to internal error', async () => {
+test('unsuccesful quote retrieval due to internal error', async () => {
     //@ts-expect-error
     expect(await (await getQuoteHistoryData()).json()).toEqual({
         success: false,
         message: "Request failed due to error"
     } as GeneralAPIResponse);
-})
+});
