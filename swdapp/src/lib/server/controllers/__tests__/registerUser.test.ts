ec6745aa19b04e08c9e2323d6d9c6a4ed5fb667b
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-11 22:19:30 -0500

    refactored registerUser unit tests to include mock functions

--- swdapp/src/lib/server/controllers/__tests__/registerUser.test.ts
+++ swdapp/src/lib/server/controllers/__tests__/registerUser.test.ts
@@ -1,40 +1,60 @@
-import { expect, test } from 'vitest';
+import { afterAll, expect, test, vi } from "vitest";
+import { registerUser } from "../registerController";
 
-import { registerUser } from '../registerController';
-import type { RegistrationRequest, RegistrationResponse } from '$lib/server/customTypes/authTypes';
+import * as UserService from '../../services/userService';
+import type { RegistrationRequest, RegistrationResponse } from "$lib/server/customTypes/authTypes";
 
-test.skip('unsuccesful registration where a user already exists', async () => {
+const userExistsSpy = vi.spyOn(UserService, 'userExists');
+const addUserSpy = vi.spyOn(UserService, 'addUser');
+
+afterAll(() => {
+    userExistsSpy.mockRestore();
+    addUserSpy.mockRestore();
+});
+
+test('unsuccesful registration because the user already exists', async () => {
     const testRequest: RegistrationRequest = {
-        username: 'dummyUser1',
+        username: 'registeredUser',
         password: 'pass1'
-    }
+    };
 
-    expect(await (await registerUser(testRequest)).json()).toEqual({
+    (userExistsSpy as any).mockImplementation(async () => { return true; });
+
+    const res = await registerUser(testRequest);
+    const resJSON: RegistrationResponse = await res.json();
+
+    expect(resJSON).toEqual({
         success: false,
         response: {
             failType: 'exists',
-            message: 'dummyUser1 is already registered'
+            message: `${testRequest.username} is already registered`
         }
     } as RegistrationResponse);
-})
+});
 
-test.skip('succesful registration of a new user', async () => {
+test('succesful registration of a new user', async () => {
     const newRandomUserName = Math.random().toString(36).substring(2,7) + '_' + Date.now().toString();
     console.log(`Testing new registration for test user: ${newRandomUserName}`);
 
     const testRequest: RegistrationRequest = {
         username: newRandomUserName,
-        password: 'unsecurePassword'
-    }
+        password: 'newPassword'
+    };
 
-    expect(await (await registerUser(testRequest)).json()).toEqual({
+    userExistsSpy.mockImplementation(async () => { return false; });
+    (addUserSpy as any).mockImplementation( async () => { return true; });
+
+    const res = await registerUser(testRequest);
+    const resJSON: RegistrationResponse = await res.json();
+
+    expect(resJSON).toEqual({
         success: true,
         response: `Succesfully registered ${newRandomUserName}`
     } as RegistrationResponse);
-})
+});
 
-test.skip('registration fails due to network error or undefined info', async () => {
-    // @ts-expect-error
+test('registration fails due to network error or undefined info', async () => {
+    //@ts-expect-error
     expect(await (await registerUser()).json()).toEqual({
         success: false,
         response: {
@@ -42,4 +62,4 @@ test.skip('registration fails due to network error or undefined info', async () 
             message: "Request failed due to error"
         }
     } as RegistrationResponse);
-})
+});
