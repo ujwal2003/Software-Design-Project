24293620477ccf47960e53d40b12c87d713033cd
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-11 21:16:30 -0500

    implemented mock functions in logout unit test

--- swdapp/src/lib/server/controllers/__tests__/logout.test.ts
+++ swdapp/src/lib/server/controllers/__tests__/logout.test.ts
@@ -1,4 +1,10 @@
-import { beforeAll, vi } from "vitest";
+import { afterAll, beforeAll, expect, test, vi } from "vitest";
+import { logOutUser } from "../authController";
+
+import * as AuthService from '../../services/authorizationService';
+import type { LogOutRequest, LogOutResponse } from "$lib/server/customTypes/authTypes";
+
+const revokeRefTokenSpy = vi.spyOn(AuthService, 'revokeRefreshToken');
 
 beforeAll(() => {
     vi.mock('$env/static/private', () => {
@@ -7,4 +13,25 @@ beforeAll(() => {
             ACCESS_TOKEN_SECRET: 'test2',
         };
     });
+});
+
+afterAll(() => {
+    revokeRefTokenSpy.mockRestore();
+});
+
+test('successfull user logout', async () => {
+    const testRequest: LogOutRequest = {
+        username: 'dummyUser',
+        refreshToken: ''
+    };
+
+    revokeRefTokenSpy.mockImplementation(async () => { return; });
+
+    const res = await logOutUser(testRequest);
+    const resJSON = await res.json();
+
+    expect(resJSON).toEqual({
+        success: true,
+        message: `Successfully logged out user ${testRequest.username}`
+    } as LogOutResponse);
 });
