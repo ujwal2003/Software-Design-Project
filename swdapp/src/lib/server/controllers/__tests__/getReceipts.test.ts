4bcf449b557a3d468766c9cf15e06461bd63ced6
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-12 13:36:05 -0500

    refactored get receipts unit tests to use mock functions

--- swdapp/src/lib/server/controllers/__tests__/getReceipts.test.ts
+++ swdapp/src/lib/server/controllers/__tests__/getReceipts.test.ts
@@ -1,9 +1,18 @@
-import { beforeAll, expect, test, vi } from 'vitest';
-
+import { afterAll, beforeAll, expect, test, vi } from 'vitest';
+import * as bcrypt from "bcrypt";
+import crypto from "crypto";
+import * as UserService from '../../services/userService';
+import * as AuthService from "../../services/authorizationService";
+import * as PaymentService from "../../services/paymentService";
 import { getReceipts } from '../profileController';
 import type { QuoteHistoryRequest, GeneralAPIResponse, UnauthorizedResponse, PurchaseHistoryRequest, PurchaseHistoryResponse } from '$lib/server/customTypes/generalTypes';
 import type { LoginRequest, LoginResponse, LoginSuccess } from '$lib/server/customTypes/authTypes';
 import { loginUser } from '../authController';
+
+const userExistsSpy = vi.spyOn(UserService, 'userExists');
+const getCredsSpy = vi.spyOn(AuthService, 'getUserCredentials');
+const addRefTokenSpy = vi.spyOn(AuthService, 'addUserRefreshSession');
+const receiptsSpy = vi.spyOn(PaymentService, 'getPurchaseHistory');
 
 beforeAll(() => {
     vi.mock('$env/static/private', () => {
@@ -12,126 +21,107 @@ beforeAll(() => {
             ACCESS_TOKEN_SECRET: 'test2'
         }
     });
-})
+});
 
-test.skip('successful purchase history test', async () => {
-    const testLoginRequest: LoginRequest = {
-        username: 'dummyUser4',
-        password: 'unsecurePassword4'
-    }
+afterAll(() => {
+    userExistsSpy.mockRestore();
+    getCredsSpy.mockRestore();
+    addRefTokenSpy.mockRestore();
+    receiptsSpy.mockRestore();
+});
+
+test('successful purchase history retrieval', async () => {
+    const testLoginRequest: LoginRequest = { username: 'dummyUser', password: 'pass1' };
+
+    const salt = await bcrypt.genSalt();
+    const hashedPass = await bcrypt.hash(testLoginRequest.password, salt);
+
+    (userExistsSpy as any).mockImplementation(async () => { return true; });
+    getCredsSpy.mockImplementation(async () => { return { username: 'user1', encryptedPass: hashedPass }; });
+    addRefTokenSpy.mockImplementation(async () => { return; });
 
     const loginRes: LoginResponse<LoginSuccess> = await (await loginUser(testLoginRequest)).json();
+    expect(loginRes.success).toBeTruthy();
 
     const testRequest: PurchaseHistoryRequest = {
-        username: 'dummyUser4',
+        username: testLoginRequest.username,
         accessToken: loginRes.response.accessToken
-    }
+    };
 
-    expect(await (await getReceipts(testRequest)).json()).toEqual({
-        success: true,
-        purchaseHistory: [
-            {
-                _id: "4319cc4aaff141ff0d93ffb2",
-                quoteID: "311650601c91eb0501b75a98",
-                purchaseDate: "2024-03-10T00:00:00.000Z",
-                deliveryDate: "2024-03-11T00:00:00.000Z",
-                tax: 2.10,
-                price: 25.23
-            },
-            {
-                _id: "aeae92d4b6478cb65d2fc6c8",
-                quoteID: "b991d60266e4a73503571c61",
-                purchaseDate: "2024-03-18T00:00:00.000Z",
-                deliveryDate: "2024-03-19T00:00:00.000Z",
-                tax: 4.52,
-                price: 10.24
-            },
-            {
-                _id: "777287681d68d56ebac8e635",
-                quoteID: "2593b58ba4dc28e2b4e1edbd",
-                purchaseDate: "2024-03-22T00:00:00.000Z",
-                deliveryDate: "2024-03-24T00:00:00.000Z",
-                tax: 5.72,
-                price: 300.21
-            }
-        ] 
+    (receiptsSpy as any).mockImplementation(async () => {
+        return [
+            { quoteID: crypto.randomBytes(24 / 2).toString('hex'), purchaseDate: "2024-03-10T00:00:00.000Z", deliveryDate: "2024-03-11T00:00:00.000Z", tax: 2.10, price: 25.23 },
+            { quoteID: crypto.randomBytes(24 / 2).toString('hex'), purchaseDate: "2024-03-18T00:00:00.000Z", deliveryDate: "2024-03-19T00:00:00.000Z", tax: 2.10, price: 25.23 },
+            { quoteID: crypto.randomBytes(24 / 2).toString('hex'), purchaseDate: "2024-03-22T00:00:00.000Z", deliveryDate: "2024-03-24T00:00:00.000Z", tax: 2.10, price: 25.23 }
+        ];
     });
-})
 
-test.skip('successful purchase history test (empty / no quotes)', async () => {
-    const testLoginRequest: LoginRequest = {
-        username: 'dummyUser3',
-        password: 'unsecurePassword3'
-    }
+    const res = await getReceipts(testRequest);
+    const resJSON = await res.json();
 
-    const loginRes: LoginResponse<LoginSuccess> = await (await loginUser(testLoginRequest)).json();
+    expect(resJSON.success).toBeTruthy();
+    expect(resJSON.purchaseHistory).toBeDefined();
+    expect(resJSON.purchaseHistory.length).toEqual(3);
+});
 
-    const testRequest: PurchaseHistoryRequest = {
-        username: 'dummyUser3',
-        accessToken: loginRes.response.accessToken
-    }
+test('successful purchase history retrieval with no receipts', async () => {
+    const testLoginRequest: LoginRequest = { username: 'dummyUser', password: 'pass1' };
 
-    expect(await (await getReceipts(testRequest)).json()).toEqual({
-            success: true,
-            purchaseHistory: [] 
-    } as PurchaseHistoryResponse);
-})
+    const salt = await bcrypt.genSalt();
+    const hashedPass = await bcrypt.hash(testLoginRequest.password, salt);
 
-test.skip('purchase history not found test', async () => {
-    const testLoginRequest: LoginRequest = {
-        username: 'dummyUser1',
-        password: 'unsecurePassword1'
-    }
+    (userExistsSpy as any).mockImplementation(async () => { return true; });
+    getCredsSpy.mockImplementation(async () => { return { username: 'user1', encryptedPass: hashedPass }; });
+    addRefTokenSpy.mockImplementation(async () => { return; });
 
     const loginRes: LoginResponse<LoginSuccess> = await (await loginUser(testLoginRequest)).json();
+    expect(loginRes.success).toBeTruthy();
 
     const testRequest: PurchaseHistoryRequest = {
-        username: 'dummyUser1',
+        username: testLoginRequest.username,
         accessToken: loginRes.response.accessToken
-    }
+    };
 
-    expect(await (await getReceipts(testRequest)).json()).toEqual({
-        success: false,
-        message: "Purchase history not found"
-    } as GeneralAPIResponse);
-})
+    (receiptsSpy as any).mockImplementation(async () => { return [] });
 
-test.skip('purchase history not found test', async () => {
-    const testLoginRequest: LoginRequest = {
-        username: 'dummyUser1',
-        password: 'unsecurePassword1'
-    }
+    const res = await getReceipts(testRequest);
+    const resJSON = await res.json();
+
+    expect(resJSON.success).toBeTruthy();
+    expect(resJSON.purchaseHistory.length).toEqual(0);
+});
+
+test('failure due to profile not found', async () => {
+    const testLoginRequest: LoginRequest = { username: 'dummyUser', password: 'pass1' };
+
+    const salt = await bcrypt.genSalt();
+    const hashedPass = await bcrypt.hash(testLoginRequest.password, salt);
+
+    (userExistsSpy as any).mockImplementation(async () => { return true; });
+    getCredsSpy.mockImplementation(async () => { return { username: 'user1', encryptedPass: hashedPass }; });
+    addRefTokenSpy.mockImplementation(async () => { return; });
 
     const loginRes: LoginResponse<LoginSuccess> = await (await loginUser(testLoginRequest)).json();
+    expect(loginRes.success).toBeTruthy();
 
     const testRequest: PurchaseHistoryRequest = {
-        username: 'dummyUserNew',
+        username: testLoginRequest.username,
         accessToken: loginRes.response.accessToken
-    }
+    };
 
-    expect(await (await getReceipts(testRequest)).json()).toEqual({
-        success: false,
-        message: "Purchase history not found"
-    } as GeneralAPIResponse);
-})
+    receiptsSpy.mockImplementation(async () => { return null });
 
-test.skip('unsuccesful quote retrieval due to invalid access token', async () => {
-    const testRequest: QuoteHistoryRequest = {
-        username: 'dummyUser3',
-        accessToken: ''
-    }
+    const res = await getReceipts(testRequest);
+    const resJSON: GeneralAPIResponse = await res.json();
 
-    expect(await (await getReceipts(testRequest)).json()).toEqual({
-        success: true,
-        unauthorized: true,
-        message: 'invalid access token'
-    } as UnauthorizedResponse);
-})
+    expect(resJSON.success).toBeFalsy();
+    expect(resJSON.message).toEqual("Purchase history not found");
+});
 
-test.skip('unsuccesful quote retrieval due to internal error', async () => {
+test('unsuccesful retrieval due to internal error', async () => {
     //@ts-expect-error
     expect(await (await getReceipts()).json()).toEqual({
         success: false,
         message: "Request failed due to error"
     } as GeneralAPIResponse);
-})
+});
