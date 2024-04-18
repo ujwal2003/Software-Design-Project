6a0f9ca99c1bbb9ec116b1d32ec960f536106080
ujwal2003 <joshiujwal2003@gmail.com>
2024-04-18 13:23:15 -0500

    added profile schema

---
+++ swdapp/src/lib/server/database/models/profileModel.ts
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const receiptSchema = new mongoose.Schema(
+    {
+        quoteID: { 
+            type: String,
+            required: [true, "quoteID is required!"] 
+        },
+
+        purchaseDate: { 
+            type: Date,
+            required: [true, "purchaseDate is required!"] 
+        },
+
+        deliveryDate: { 
+            type: Date,
+            required: [true, "deliveryDate is required!"] 
+        },
+
+        tax: { 
+            type: Number,
+            required: [true, "tax is required!"] 
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const paymentInfoSchema = new mongoose.Schema(
+    {
+        cardName: { type: String },
+        creditCardNumber: { type: String },
+        cardExpiration: { type: Date },
+        cardCVV: { type: String }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const profileSchema = new mongoose.Schema(
+    {
+        firstName: { type: String },
+        middleName: { type: String },
+        lastName: { type: String },
+        street: { type: String },
+        state: { type: String },
+        city: { type: String },
+        zip: { type: String },
+
+        purchaseHistory: [receiptSchema],
+
+        paymentInfo: {
+            type: paymentInfoSchema,
+            default: null
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const ProfileModel = mongoose.model('Profile', profileSchema);
